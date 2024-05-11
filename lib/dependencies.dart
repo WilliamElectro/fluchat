90a6556ddaf90171a3a0f47c83efd9a5ae2e6504
wbohorquez <wi-lli-am-3@hotmail.com>
2024-05-11 15:19:40 -0500

    Add cloud functions

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -25,13 +25,13 @@ List<RepositoryProvider> buildRepositories(StreamChatClient client) {
     //Prod
     RepositoryProvider<AuthRepository>(create: (_) => AuthImpl()),
     RepositoryProvider<PersistentStorageRepository>(create: (_) => PersistentStorageImpl()),
-    RepositoryProvider<StreamApiRepository>(create: (_) => StreamApiImpl(client)),
+    //RepositoryProvider<StreamApiRepository>(create: (_) => StreamApiImpl(client)),
     RepositoryProvider<UploadStorageRepository>(create: (_) => UploadStorageImpl()),
     /**Local
     RepositoryProvider<AuthRepository>(create: (_) => AuthLocalImpl()),
-    RepositoryProvider<PersistentStorageRepository>(create: (_) => PersistentStorageLocalImpl()),
+    RepositoryProvider<PersistentStorageRepository>(create: (_) => PersistentStorageLocalImpl()),**/
     RepositoryProvider<StreamApiRepository>(create: (_) => StreamApiLocalImpl(client)),
-    RepositoryProvider<UploadStorageRepository>(create: (_) => UploadStorageLocalImpl()),**/
+    //RepositoryProvider<UploadStorageRepository>(create: (_) => UploadStorageLocalImpl()),**/
 
     RepositoryProvider<ImagePickerRepository>(create: (_) => ImagePickerImpl()),
     RepositoryProvider<ProfileSignInUseCase>(
